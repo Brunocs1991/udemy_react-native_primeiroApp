c0177b1c4d58512ec117eeca0ef6a0cd86565ed7
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-03-08 10:18:34 -0300

    criando componentes e passando props

--- App.tsx
+++ App.tsx
@@ -1,22 +1,9 @@
 import React, {Component} from 'react';
-import {Image, Text, View} from 'react-native';
+import Inicial from './src/Inicial.tsx';
 
 class App extends Component {
   render() {
-    let nome = 'Bruno';
-    let img = 'https://sujeitoprgramador.com/steve.svg';
-
-    return (
-      <View>
-        <Text>Olá Mundo!!!</Text>
-        <Text>Meu Primeiro APP</Text>
-        <Text style={{color: '#FF0000', fontSize: 25, margin: 15}}>
-          Sujeito Programador
-        </Text>
-        <Image source={{uri: img}} style={{width: 300, height: 300}} />
-        <Text style={{fontSize: 30}}>{nome}</Text>
-      </View>
-    );
+    return <Inicial />;
   }
 }
 
---
+++ src/Inicial.tsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+import {Image, Text, View} from 'react-native';
+import Jobs from './components/Jobs.tsx';
+
+class Inicial extends Component {
+  render() {
+    let nome = 'Bruno';
+
+    return (
+      <View>
+        <Text>Olá Mundo!!!</Text>
+        <Text>Meu Primeiro APP</Text>
+        <Text style={{color: '#FF0000', fontSize: 25, margin: 15}}>
+          Sujeito Programador
+        </Text>
+        <Text style={{fontSize: 30}}>{nome}</Text>
+        <Jobs largura={500} altura={200} nome="Steve Jobs" />
+      </View>
+    );
+  }
+}
+
+export default Inicial;
