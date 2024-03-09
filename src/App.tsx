3e8fb6e0d9123a389ad447bcde5c09aca379aee1
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-03-09 19:33:51 -0300

    Refatoring, and state

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import Jobs from './components/Jobs.tsx';
+import Inicial from './Screen/Inicial.tsx';
+import State from './Screen/State.tsx';
 
 class App extends Component {
   render() {
-    let nome = 'Bruno';
-
     return (
-      <View>
-        <Text>Olá Mundo!!!</Text>
-        <Text>Meu Primeiro APP</Text>
-        <Text style={{color: '#FF0000', fontSize: 25, margin: 15}}>
-          Sujeito Programador
-        </Text>
-        <Text style={{fontSize: 30}}>{nome}</Text>
-        <Jobs largura={500} altura={200} nome="Steve Jobs" />
-      </View>
+      <>
+        {/*<Inicial />*/}
+        <State />
+      </>
     );
   }
 }
